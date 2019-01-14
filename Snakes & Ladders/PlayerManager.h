37ee5bd65dfff6bc9298038157ae48385807714c
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-13 16:30:25 -0800

    add player manager

---
+++ Snakes & Ladders/PlayerManager.h
@@ -0,0 +1,17 @@
+//
+//  PlayerManager.h
+//  Snakes & Ladders
+//
+//  Created by jason harrison on 2019-01-13.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlayerManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Snakes & Ladders/PlayerManager.m
@@ -0,0 +1,13 @@
+//
+//  PlayerManager.m
+//  Snakes & Ladders
+//
+//  Created by jason harrison on 2019-01-13.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import "PlayerManager.h"
+
+@implementation PlayerManager
+
+@end
